46649fd7003069cef4578a1a2bb62e4771c250b7
Jonathan Querubina <jotadev@me.com>
2016-11-20 09:40:50 -0200

    Add reviewer

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from oauth.models import Client
 from reviews.models import Company
 from reviews.models import Reviewer
+from django.views.decorators.csrf import csrf_exempt
 
 # get a list of companies
 def companies(request):
@@ -19,15 +20,27 @@ def companies(request):
     return JsonResponse({'error': 'Http method not allowed'})
 
 # get a list of reviewers
+@csrf_exempt
 def reviewers(request):
-    if request.method == 'GET':
-        token = request.META['HTTP_X_AUTHORIZATION']
-        if token:
-            client = Client.objects.filter(token=token).first()
-            if client is not None:
-                reviewers = Reviewer.objects.all()
+    token = request.META['HTTP_X_AUTHORIZATION']
+    if token:
+        client = Client.objects.filter(token=token).first()
+        if client is not None:
+            if request.method == 'GET':
+                reviewers = Reviewer.objects.filter(client=client)
                 reviewers = [{"id": r.pk, "name": r.name} for r in reviewers]
                 return JsonResponse({"reviewers": reviewers})
-            return JsonResponse({"error": "A valid token is needed for this request."})
+            elif request.method == 'POST':
+                name = request.POST.get('name','')
+                if name:
+                    reviewer = Reviewer.objects.create(name=name, client=client)
+                    reviewer = {
+                        "id": reviewer.pk,
+                        "name": reviewer.name
+                    }
+                    return JsonResponse({'reviewer': reviewer})
+                return JsonResponse({'error': 'Missing name field'})
+            else:
+                return JsonResponse({'error': 'Http method not allowed'})
         return JsonResponse({"error": "A valid token is needed for this request."})
-    return JsonResponse({'error': 'Http method not allowed'})
+    return JsonResponse({"error": "A valid token is needed for this request."})
