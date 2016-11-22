ec391c642334ef37eca565fbd6df30ef80a256d5
Jonathan Querubina <jotadev@me.com>
2016-11-22 08:37:33 -0200

    Review logic

--- reviews/decorators.py
+++ reviews/decorators.py
@@ -1,14 +1,19 @@
 from django.utils.functional import wraps
 from rest_framework import status
 from rest_framework.response import Response
+from oauth.models import Client
 
 
 def token_required():
     def decorator(func):
         def inner_decorator(request, *args, **kwargs):
             token = request.META.get('HTTP_X_AUTHORIZATION', '')
-            if len(token) == 36:
-                return func(request, *args, **kwargs)
-            return Response({"error": "This endpoint requires a token"}, status.HTTP_403_FORBIDDEN)
+            if len(token) != 36:
+                return Response({"error": "Invalid token"}, status.HTTP_403_FORBIDDEN)
+            client = Client.objects.filter(token=token).first()
+            if client is None:
+                return Response({"error": "Client does not exists"}, status=status.HTTP_401_UNAUTHORIZED)
+            request.client = client
+            return func(request, *args, **kwargs)
         return wraps(func)(inner_decorator)
     return decorator
--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,6 @@
-import re
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from oauth.models import Client
 from .models import Company
 from .models import Reviewer
 from .models import Review
@@ -19,59 +15,47 @@ from .serializers import ReviewSerializer
 @api_view(['GET'])
 @token_required()
 def companies(request):
-    token = request.META['HTTP_X_AUTHORIZATION']
-    client = Client.objects.filter(token=token).first()
-    if client is not None:
-        serializer = CompanySerializer(Company.objects.all(), many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response({"error": "client does not exists"}, status=status.HTTP_403_FORBIDDEN)
+    serializer = CompanySerializer(Company.objects.all(), many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'POST'])
 @token_required()
 def reviewers(request):
-    token = request.META['HTTP_X_AUTHORIZATION']
-    client = Client.objects.filter(token=token).first()
-    if client is not None:
-        if request.method == 'GET':
-            serializer = ReviewerSerializer(Reviewer.objects.filter(client=client), many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        elif request.method == 'POST':
-            data = {
-                "name": request.data.get('name'),
-                "email": request.data.get('email'),
-                "client": client.username,
-            }
-            serializer = ReviewerSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        serializer = ReviewerSerializer(Reviewer.objects.filter(client=request.client), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        data = {
+            "name": request.data.get('name'),
+            "email": request.data.get('email'),
+            "client": request.client.username,
+        }
+        serializer = ReviewerSerializer(data=data)
+        if not serializer.is_valid():
             return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({"error": "client does not exists"}, status=status.HTTP_403_FORBIDDEN)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'POST'])
 @token_required()
 def reviews(request):
-    token = request.META['HTTP_X_AUTHORIZATION']
-    client = Client.objects.filter(token=token).first()
-    if client is not None:
-        if request.method == 'GET':
-            serializer = ReviewSerializer(Review.objects.filter(client=client), many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        elif request.method == 'POST':
-            data = {
-                "rating": request.data.get('rating'),
-                "title": request.data.get('title'),
-                "summary": request.data.get('summary'),
-                "ip": get_ip(request),
-                "company": request.data.get('company_id'),
-                "reviewer": request.data.get('reviewer_id'),
-                "client": client.username
-            }
-            serializer = ReviewSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        serializer = ReviewSerializer(Review.objects.filter(client=request.client), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        data = {
+            "rating": request.data.get('rating'),
+            "title": request.data.get('title'),
+            "summary": request.data.get('summary'),
+            "ip": get_ip(request),
+            "company": request.data.get('company_id'),
+            "reviewer": request.data.get('reviewer_id'),
+            "client": request.client.username
+        }
+        serializer = ReviewSerializer(data=data)
+        if not serializer.is_valid():
             return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({"error": "client does not exists"}, status=status.HTTP_403_FORBIDDEN)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
